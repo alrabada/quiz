33f9a2541f94fd2b5ddf0a7768f95b82fd1af1b3
Angel Lopez <alrabada@gmail.com>
2015-07-15 19:28:26 +0200

    Version 8: Problemas despliegue Heroku con Postgres

--- models/models.js
+++ models/models.js
@@ -44,7 +44,7 @@ sequelize.sync().then(function() {
   // success(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().then(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
-      Quiz.create({ pregunta: 'Capital de Italia SQLite',
+      Quiz.create({ pregunta: 'Capital de Italia',
       	            respuesta: 'Roma'
       	         })
      .then(function(){console.log('Base de datos inicializada')});
