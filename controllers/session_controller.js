a09f72f1a693886ae1e0cca44ee8d766f7a90a7e
Angel Lopez <alrabada@gmail.com>
2015-07-26 19:27:25 +0200

    version 18: Autorizacion

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,14 @@
+//MW de autorizacion de accesosHTTP restringdos
+exports.loginRequired = function(req, res, next){
+  if (req.session.user){
+    next();
+
+  }else{
+    res.redirect('/login');
+  }
+};
+
+
 // GET /login -- formulario de login
 exports.new = function(req,res){
 	var errors = req.session.errors || {};
--- routes/index.js
+++ routes/index.js
@@ -14,18 +14,18 @@ router.get('/', function(req, res) {
 router.param('quizId', quizController.load);
 
 // Definición de rutas de /quizes
-router.get('/quizes', quizController.index);
-router.get('/quizes/:quizId(\\d+)', quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizes/new',quizController.new);
-router.post('/quizes/create',quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
-router.put('/quizes/:quizId(\\d+)', quizController.update);
-router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+router.get('/quizes', 						quizController.index);
+router.get('/quizes/:quizId(\\d+)', 		quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
+router.get('/quizes/new', 					sessionController.loginRequired, quizController.new);
+router.post('/quizes/create', 				sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', 	sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizController.destroy);
 
 // Definición de rutas de /comments
-router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
-router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/new', 	commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments', 		commentController.create);
 
 // Definición de rutas de session
 router.get('/login', sessionController.new); //formulario login
